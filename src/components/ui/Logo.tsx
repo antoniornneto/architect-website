4fb151ad858c4e6eec0404a81f496a0b81e0118b
Antonio Nascimento <antonionascneto@gmail.com>
2025-02-05 13:39:24 -0300

    feat: logo now is a link for home page

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import logo from "../../../assets/logo.png";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div className="px-2">
+    <Link href={"/"} className="px-2">
       <Image src={logo} width={100} alt="logo" className="object-cover" />
-    </div>
+    </Link>
   );
 };
 
